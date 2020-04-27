cdafdd7200a155bce85502f2ccf4b33bf20aab4e
santoni98 <albertgateev1@mail.ru>
2020-04-27 19:47:12 +0300

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Its fork";
+    return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"rrrrrrrrrrrrrrrrrrrrrrrrrrrrrr";
+    return 0;
+}
