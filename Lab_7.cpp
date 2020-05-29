10730f7d46d01a46ff9ab5fa80afb44b0218c9d7
santoni98 <albertgateev1@mail.ru>
2020-05-29 12:30:41 +0300

    v1

--- Lab_7.cpp
+++ Lab_7.cpp
@@ -8,13 +8,12 @@ int main(){
     //Массив с диапозоном значений [11-90]
     // Выделение памяти под массив
     int a = 8,
-        c = 40,
+        c = 80,
         b = 10;
-
     // Выделение памяти под массив
     int* arr2;
     arr2 = new int[c];
-
+    //Создание и вывод массива
     int arr[8][10] = { 36, 69, 60, 21, 27, 13, 74, 25, 15, 61,
                        75, 54, 63, 55, 40, 76, 38, 44, 29, 77,
                        85, 39, 62, 30, 59, 79, 33, 37, 82, 73,
@@ -23,54 +22,87 @@ int main(){
                        86, 32, 56, 89, 47, 26, 78, 17, 81, 19,
                        80, 52, 65, 49, 42, 83, 50, 87, 72, 11,
                        68, 45, 51, 46, 58, 35, 90, 57, 48, 12 };
-    int x;
-    cout << "Введите число в диапозоне [11-90] >> ";
-    cin >> x;
-
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 10; j++) {
-            if (arr[i][j] == x && j%2!=0) {
-                cout << endl<<"Число находится в "<<j+1<<" столбце, в "<<i+1<<" строке"<<endl;
-                break;
-            }
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    //Принятие массива входных значений
+    cout<<"Количество элементов в последовательности: ";
+    int g; cin>>g;
+    if(cin.fail()||g<1||g>40){
+        cout<<"Введено некорректное значение";
+        return 0;
+    }
+    int* arrin;
+    arrin = new int[g];
+    cout<<"Введите элементы через пробел: ";
+    for(int i=0; i<g; i++){
+        cin>>arrin[i];
+        if(cin.fail()){
+            cout<<"Введено некорректное значение";
+            return 0;
         }
     }
 
+    cout<<endl<<endl<<"Поиск последовательным методом:"<<endl;
+    cout<<endl;
 
-    cout<<endl<<endl<<"Поиск бинарным методом"<<endl;
+    bool check=false;
+    for(int k=0; k<g; k++){
+        for(int i=0; i<8; i++){
+            for(int j=0; j<10; j++){
+                if(arrin[k]==arr[i][j]){
+                    cout<<"Элемент "<<arrin[k]<<" найден. Находится в ячейке ["<<i<<"]["<<j<<"]"<<endl;
+                    check=true;
+                }
+            }
+        }
+        if(check==false){
+            cout<<"Элемента "<<arrin[k]<<" нет в массиве"<<endl;
+        }
+        check=false;
+    }
+
+
+    cout<<endl<<"======================================================================="<<endl
+    <<endl;
     int w = 0;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 10; j++) {
-            if (j % 2 != 0) {
-                arr2[w] = arr[i][j];
-                //cout<<arr2[w]<<" ";
-                w++;
-            }
+            arr2[w] = arr[i][j];
+            w++;
         }
     }
+    cout<<"Поиск бинарным методом"<<endl<<endl;
 
-    bubbleSort(arr2,40);
+    bubbleSort(arr2,80);
     cout<<"Отсортированный массив:"<<endl;
-    for(int i=0;i<40; i++){
+    for(int i=0;i<80; i++){
         cout<<arr2[i]<<" ";
     }
-    bool flag = false;
-    int key=x;
-    int l = 0; // левая граница
-    int r = 39; // правая граница
-    int mid;
+    cout<<endl;
 
-    while ((l <= r) && (flag != true)) {
-      mid = (l + r) / 2;
-
-      if (arr2[mid] == key) flag = true;
-      if (arr2[mid] > key) r = mid - 1;
-      else l = mid + 1;
+    for(int p=0; p<g; p++){
+        bool flag = false;
+        int l = 0; // левая граница
+        int r = 79; // правая граница
+        int mid=0;
+        while ((l <= r) && (flag != true)) {
+            mid = (l + r) / 2;
+            if (arr2[mid] == arrin[p])
+                flag = true;
+            if (arr2[mid] > arrin[p])
+                r = mid - 1;
+            else l = mid + 1;
+        }
+        if (flag) {
+            cout << "Индекс элемента " << arrin[p] << " в массиве равен: " << mid<<endl;
+        }else{
+            cout << "Введенный элемент не найден"<<endl;
+        }
     }
-
-    if (flag) cout << "Индекс элемента " << key << " в массиве равен: " << mid;
-    else cout << "Извините, но такого элемента в массиве нет";
-
     return 0;
 }
 
